17a88558c0d0e2c295b046d470232e9d89e2d654
Leandro Antunes <lele.tune@gmail.com>
2022-07-27 15:13:33 -0300

    Theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  'background': '#FAFAFA',
+  'white': '#FFFFFF',
+  
+  'base-title': '#272221',
+  'base-subtitle': '#403937',  
+  'base-text': '#574F4D',
+  'base-label': '#8D8686',
+  
+  'base-hover': '#D7D5D5',
+  'base-button': '#E6E5E5',
+  'base-input':'#EDEDED',
+  'base-card':'#F3F2F2',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-400': '#DBAC2C',
+  'yellow-900': '#C47F17',
+
+  'purple-800': '##4B2995',
+  'purple-400':'#8047F8',
+  'purple-100': '#EBE5F9'
+}
