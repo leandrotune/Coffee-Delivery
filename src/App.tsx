fa9df9d647196026e9d8cd8a20bd906f84c7ed60
Leandro Antunes <lele.tune@gmail.com>
2022-07-27 15:53:37 -0300

    adicionando o eslint na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-export function App() {
-  return <h1>Hello word!</h1>
-}
+// import { GlobalStyle } from "./styles/global";
 
+export function App() {
+  return (
+    <>
+      <h1>Hello word!</h1>
+      {/* <GlobalStyle /> */}
+    </>
+  )
+}
 /* 
 font-family: 'Baloo 2', cursive;
 font-family: 'Roboto', sans-serif;
