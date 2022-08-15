d52295151b949cc23af93ed7c811d5d9f513cc9b
Leandro Antunes <leandro.tune@hotmail.com>
2022-08-15 19:25:50 -0300

    Componente: DeliveryAddress

---
+++ src/pages/Checkout/DeliveryAddress/index.tsx
@@ -0,0 +1,86 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  DotsThreeOutlineVertical,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
+import {
+  FormContainer,
+  FormInforContent,
+  InputInfor,
+  InputInforCidade,
+  InputInforComplemento,
+  InputInforEstado,
+  InputInforRoad,
+} from '../styles'
+import {
+  FooterContainer,
+  FooterContent,
+  SectionContainer,
+  SectionHeaderContainer,
+} from './styles'
+
+export function DeliveryAddress() {
+  return (
+    <main>
+      <header>
+        <h3>Complete seu perdido</h3>
+      </header>
+      <SectionContainer>
+        <SectionHeaderContainer>
+          <MapPinLine size={22} weight="regular" color="#C47F17" />
+          <div>
+            <h3>Endereço de Entrega</h3>
+            <p>Informe o endereço onde deseja receber seu pedido</p>
+          </div>
+        </SectionHeaderContainer>
+        <FormContainer>
+          <InputInfor type="text" placeholder="CEP" />
+
+          <InputInforRoad type="text" name="Rua" placeholder="Rua" />
+
+          <FormInforContent>
+            <InputInfor type="text" placeholder="número" />
+
+            <InputInforComplemento type="text" placeholder="complemento" />
+          </FormInforContent>
+          <FormInforContent>
+            <InputInfor type="text" placeholder="Bairro" />
+
+            <InputInforCidade type="text" placeholder="Cidade" />
+
+            <InputInforEstado type="text" placeholder="UF" />
+          </FormInforContent>
+        </FormContainer>
+      </SectionContainer>
+
+      <FooterContainer>
+        <div>
+          <CurrencyDollar size={22} weight="regular" color="#8047F8" />
+          <div>
+            <h4>Pagamento</h4>
+            <p>
+              O pagamento é feito na entrega. Escolha a forma que deseja pagar
+            </p>
+          </div>
+        </div>
+        <FooterContent>
+          <button type="button">
+            <CreditCard size={16} weight="regular" color="#8047F8" />
+            <p>Cartão de crédito</p>
+          </button>
+          <button type="button">
+            <Bank size={16} weight="regular" color="#8047F8" />
+            <p>Cartão de débito</p>
+          </button>
+          <button type="button">
+            <Money size={16} weight="regular" color="#8047F8" />
+            <p>dinheiro</p>
+          </button>
+        </FooterContent>
+      </FooterContainer>
+    </main>
+  )
+}
--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,112 +1,96 @@
-import {
-  Bank,
-  CreditCard,
-  CurrencyDollar,
-  MapPin,
-  Money,
-  Trash,
-} from 'phosphor-react'
+// import {
+//   Bank,
+//   CreditCard,
+//   CurrencyDollar,
+//   MapPinLine,
+//   Money,
+//   Trash,
+// } from 'phosphor-react'
+import { DeliveryAddress } from './DeliveryAddress'
+// import {
+//   CheckoutContainer,
+//   CompleteYourLost,
+//   FormAddress,
+//   FormContainer,
+//   FormInforContent,
+//   HeaderContainer,
+//   InputInfor,
+//   InputInforCidade,
+//   InputInforComplemento,
+//   InputInforEstado,
+//   InputInforRoad,
+// } from './styles'
 
 export function Checkout() {
   return (
     <>
-      <section>
-        <h1>Complete seu perdido</h1>
-        <main>
-          <header>
-            <MapPin />
-            <div>
-              <h2>Endereço de Entrega</h2>
-              <p>Informe o endereço onde deseja receber seu pedido</p>
-            </div>
-          </header>
-          <form>
-            <div>
-              <input type="text"></input>
-            </div>
-            <div>
-              <input type="text" placeholder="nome completo"></input>
-            </div>
-            <div>
-              <input type="text" placeholder="número"></input>
-            </div>
-            <div>
-              <input type="text" placeholder="complemento"></input>
-            </div>
-            <div>
-              <input type="text" placeholder="bairro"></input>
-            </div>
-            <div>
-              <input type="text" placeholder="cidade"></input>
-            </div>
-            <div>
-              <input type="text" placeholder="sp"></input>
-            </div>
-          </form>
-        </main>
-        <footer>
-          <CurrencyDollar />
-          <div>
-            <h3>Pagamento</h3>
-            <p>
-              O pagamento é feito na entrega. Escolha a forma que deseja pagar
-            </p>
-          </div>
-          <div>
-            <div>
-              <CreditCard />
-              <p>Cartão de crédito</p>
-            </div>
-            <div>
-              <Bank />
-              <p>Cartão de débito</p>
-            </div>
-            <div>
-              <Money />
-              <p>dinheiro</p>
-            </div>
-          </div>
-        </footer>
-      </section>
-
-      <aside>
-        <div>
-          <img
-            src="/assets/expresso-tradicional.svg"
-            alt="Expresso Tradicional"
-          />
-          <p>Expresso Tradicional</p>
-          <button>
-            <Trash />
-            remover
-          </button>
-          <strong>R$ 9,90</strong>
-        </div>
-        <div>
-          <img src="/assets/expresso-americano.svg" alt="Expresso Americano" />
-          <p>Expresso Tradicional</p>
-          <button>
-            <Trash />
-            remover
-          </button>
-          <strong>R$ 9,90</strong>
-        </div>
-        <div>
-          <div>
-            <p>total de itens</p>
-            <span>R$ 29,70</span>
-          </div>
-          <div>
-            <p>Entrega R$ 3,50</p>
-            <span>R$ 3,50</span>
-          </div>
-          <div>
-            <strong>Total</strong>
-            <span>R$ 33,20</span>
-          </div>
-        </div>
-        <button type="submit">confirmar perdido</button>
-      </aside>
+      <DeliveryAddress />
     </>
   )
 }
+//         <footer>
+//           <CurrencyDollar />
+//           <div>
+//             <h3>Pagamento</h3>
+//             <p>
+//               O pagamento é feito na entrega. Escolha a forma que deseja pagar
+//             </p>
+//           </div>
+//           <div>
+//             <div>
+//               <CreditCard />
+//               <p>Cartão de crédito</p>
+//             </div>
+//             <div>
+//               <Bank />
+//               <p>Cartão de débito</p>
+//             </div>
+//             <div>
+//               <Money />
+//               <p>dinheiro</p>
+//             </div>
+//           </div>
+//         </footer>
+//       </CompleteYourLost>
+
+//       <aside>
+//         <div>
+//           <img
+//             src="/assets/expresso-tradicional.svg"
+//             alt="Expresso Tradicional"
+//           />
+//           <p>Expresso Tradicional</p>
+//           <button>
+//             <Trash />
+//             remover
+//           </button>
+//           <strong>R$ 9,90</strong>
+//         </div>
+//         <div>
+//           <img src="/assets/expresso-americano.svg" alt="Expresso Americano" />
+//           <p>Expresso Tradicional</p>
+//           <button>
+//             <Trash />
+//             remover
+//           </button>
+//           <strong>R$ 9,90</strong>
+//         </div>
+//         <div>
+//           <div>
+//             <p>total de itens</p>
+//             <span>R$ 29,70</span>
+//           </div>
+//           <div>
+//             <p>Entrega R$ 3,50</p>
+//             <span>R$ 3,50</span>
+//           </div>
+//           <div>
+//             <strong>Total</strong>
+//             <span>R$ 33,20</span>
+//           </div>
+//         </div>
+//         <button type="submit">confirmar perdido</button>
+//       </aside>
+//     </CheckoutContainer>
+//   )
+// }
